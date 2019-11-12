c85a3b2010264f4a739872dfd04ee38cd47057ed
ruk <ruk.lakhani@students.makeschool.com>
2019-11-12 12:55:16 -0800

    updates

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # backwardsPoetry
+
+<h2>Description</h2>
+
+Printing a poem backwards, randomly, and regularly using python 
+
+<h3>Poem</h3>
+
+<p>Our generation will be known for nothing.<br>
+Never will anybody say,<br>
+We were the peak of mankind.<br>
+That is wrong, the truth is<br>
+Our generation was a failure.<br>
+Thinking that<br>
+We actually succeeded<br>
+Is a waste. And we know<br>
+Living only for money and power<br>
+Is the way to go.<br>
+Being loving, respectful, and kind<br>
+Is a dumb thing to do.<br>
+Forgetting about that time,<br>
+Will not be easy, but we will try.<br>
+Changing our world for the better<br>
+Is something we never did.<br>
+Giving up<br>
+Was how we handled our problems.<br>
+Working hard<br>
+Was a joke.<br>
+We knew that<br>
+People thought we couldn’t come back<br>
+That might be true,<br>
+Unless we turn things around</p>
+
---
+++ backwardsPoem.py
@@ -0,0 +1,62 @@
+import random as random
+
+poem ="""Our generation will be known for nothing.
+Never will anybody say,
+We were the peak of mankind.
+That is wrong, the truth is
+Our generation was a failure.
+Thinking that
+We actually succeeded
+Is a waste. And we know
+Living only for money and power
+Is the way to go.
+Being loving, respectful, and kind
+Is a dumb thing to do.
+Forgetting about that time,
+Will not be easy, but we will try.
+Changing our world for the better
+Is something we never did.
+Giving up
+Was how we handled our problems.
+Working hard
+Was a joke.
+We knew that
+People thought we couldn’t come back
+That might be true,
+Unless we turn things around"""
+
+poems = poem.split("\n")
+
+#TODO: get a list of strings that contains lines of poem
+
+def printBackwards(poems):
+    ''' Arrange poem backwards'''
+    index = len(poems)
+    poems.reverse()
+
+    for i in poems:
+        print(f'{str(index)} {i}')
+        index -= 1
+
+
+def printRandom(poem_lines_list):
+    ''' Arrange poem random  '''
+    for i in poems:
+        linesRandom = random.randint(0, (len(poems)-1)) 
+        print(poems[linesRandom])
+
+ 
+def rearrange():
+    ''' This function will print 3 random lines'''
+    for poem in poems:
+        print(poem[::-1])
+    
+
+#TODO: get a list of strings that contains lines of poem
+#Use lines = poem.split("\n")
+ 
+ 
+#Testing code
+printBackwards(poems)
+printRandom(poems)
+rearrange()
